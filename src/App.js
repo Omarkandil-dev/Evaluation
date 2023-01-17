c1c06662c5614461ddc230bac45ecd99b1adf95a
omar_kandil <93253683+Omarkandil-dev@users.noreply.github.com>
2023-01-17 03:47:32 -0800

    commit

--- src/App.js
+++ src/App.js
@@ -1,25 +1,104 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState,  useEffect} from 'react'
+import axios from 'axios'; 
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+
+
+// const App = () => {
+
+//      const [mssg, setMssg] = useState('');
+//      const [input, setInput] = useState('');
+
+//      useEffect(() => {
+//         if (input != "") {
+//           axios.get('http://127.0.0.1:8000/api/searchstagiaire/' + input).then((res) => {
+//             setMssg(res.data);
+//             console.log(res);
+//           });
+//         }
+//       }, [input]);
+
+//       const handleInput = (e) => {
+//         setInput(e.target.value);
+//       }
+
+//   return (
+//     <div>
+//         <input type="text" onChange={handleInput} placeholder="search "  value={input}/>
+//         <p>{mssg}</p>
+//     </div>
+//   )
+// }
+
+
+const App = () => {
+
+  const [result, setResult] = useState([]);
+  const [input, setInput] = useState('');
+  const [data,setData] =useState('');
+
+ 
+  const additem = (id) => {
+    axios.get('http://127.0.0.1:8000/api/update/' + id ).then((response) => {
+      // console.log(response.data);
+      setData(response.data.name);
+
+   })
+
+   let myList = {result};
+    const Element = Array.from(myList).indexOf(2)
+    console.log(Element);
+    Array.from(myList).splice(Element,0);
+  
+  }
+
+  function deleteitem(){
+    setData('');
+  }
+
+  useEffect(() => {
+
+    if (input) {
+           axios.get('http://127.0.0.1:8000/api/searchstagiaire/' + input).then((response) => {
+            setResult(response.data);     
+      });
+    }
+    else{
+           setResult([])
+    }
+  }, [input]
+  
   );
+  const handlechange = (e) => {
+    setInput(e.target.value);
+    
+  }
+
+  return (
+    <div>
+      <input type="text" onChange={handlechange} value={input} />
+      <ul>
+        {result.map((item)=>{
+          return <li>
+          {item.name} 
+          <button onClick={()=>additem(item.id)}  href="#" role="button">+</button>
+          </li>
+        })}
+      </ul>
+
+      <table>
+        <tbody>
+          <tr>
+            <td>{data} <button onClick={()=>deleteitem()}>-</button></td>
+          </tr>
+        </tbody>
+      </table>
+    </div>
+  )
 }
 
-export default App;
+
+
+ export default App
+
+
+
